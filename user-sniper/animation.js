8ff0ac2f4a8ce51beb572499883437bb603b9d34
72 <72sevenzy2@gmail.com>
2025-10-01 17:22:43 +0500

    new project

---
+++ user-sniper/animation.js
@@ -0,0 +1,17 @@
+const element = document.getElementById("animation1");
+const ctx = element.get
+
+window.addEventListener("resize", (e) => {
+});
+
+class Neutrons {
+    constructor(x, y, radius, speed) {
+        this.x = x; this.y = y; this.radius = radius, this.speed = speed;
+        this.velocityX = velocityX; this.velocityY = velocityY;
+    }
+}
+
+let portals = [];
+for (j = 0; j < 2; j++) {
+    const speed = Math.random() * 10;
+}
