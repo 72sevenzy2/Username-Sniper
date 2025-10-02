4bfb4fab334524e54ad2a254a1e8613f578329be
72 <72sevenzy2@gmail.com>
2025-10-02 05:17:23 +0500

    finished background animation

--- user-sniper/animation.js
+++ user-sniper/animation.js
@@ -1,17 +1,59 @@
-const element = document.getElementById("animation1");
-const ctx = element.get
+const canvas = document.getElementById("particleCanvas");
+const ctx = canvas.getContext("2d");
 
-window.addEventListener("resize", (e) => {
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+window.addEventListener("resize", () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 });
 
-class Neutrons {
-    constructor(x, y, radius, speed) {
-        this.x = x; this.y = y; this.radius = radius, this.speed = speed;
-        this.velocityX = velocityX; this.velocityY = velocityY;
+// Ball class
+class Ball {
+    constructor(x, y, radius, color, vx, vy) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.vx = vx;
+        this.vy = vy;
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.closePath();
+    }
+
+    update() {
+        this.x += this.vx;
+        this.y += this.vy;
+
+        // Bounce off walls
+        if (this.x - this.radius < 0 || this.x + this.radius > canvas.width) {
+            this.vx *= -1;
+        }
+        if (this.y - this.radius < 0 || this.y + this.radius > canvas.height) {
+            this.vy *= -1;
+        }
+
+        this.draw();
     }
 }
 
-let portals = [];
-for (let j = 0; j < 2; j++) {
-    const speed = Math.random() * 10;
+// Create 2 balls
+const ball1 = new Ball(300, 400, 200, "white", 3, 2);
+
+// Animation loop
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ball1.update();
+
+    requestAnimationFrame(animate);
 }
+
+animate();
